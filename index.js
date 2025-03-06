d0b00b15c8cbc0e0fcae1c95783455982521bfdc
AbhinandanRoy <abhinandancr7@gmail.com>
2025-03-06 15:01:47 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 4000;
 
 app.get('/abhinandan',(req,res)=>{
     res.send("Welcom to home Page!!")
 });
 
-app.listen(PORT,()=>{console.log(`Running at post- ${PORT}`)});
+app.listen(port,()=>{console.log(`Running at post- ${port}`)});
